345400388b709b6f1fcd9582c3158b58f90b804b
LEE MYEONG JIN <71704247+NoirNorie@users.noreply.github.com>
2022-03-23 02:43:25 +0900

    Delete Upgrade.cpp

--- Upgrade.cpp
+++
@@ -1,8 +0,0 @@
-// Copyright Epic Games, Inc. All Rights Reserved.
-
-#include "Upgrade.h"
-#include "Modules/ModuleManager.h"
-
-
-DEFINE_LOG_CATEGORY(Upgrade);
-IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, Upgrade, "Upgrade" );
