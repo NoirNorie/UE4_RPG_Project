ffd8659627e10af1946df3db9b027529f3ce980c
LEE MYEONG JIN <71704247+NoirNorie@users.noreply.github.com>
2022-03-23 02:42:48 +0900

    Add files via upload

---
+++ Upgrade.cpp
@@ -0,0 +1,8 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "Upgrade.h"
+#include "Modules/ModuleManager.h"
+
+
+DEFINE_LOG_CATEGORY(Upgrade);
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, Upgrade, "Upgrade" );
